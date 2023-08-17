6d4f2ab82f04405a6dd24345e98b0b35933ab6c0
Anikeet-dev <aniketkakde55@.com>
2023-08-17 08:36:44 +0530

    Added user delete feature

--- app.js
+++ app.js
@@ -4,19 +4,18 @@ const lastName = document.querySelector('#lname');
 const genderInput = document.querySelector('#gender');
 const dateInput = document.querySelector('#date');
 const contactInput = document.querySelector('#phNumber');
-const userBox = document.getElementById('user-box');
-
+const userList = document.getElementById('users');
 
 
 myForm.addEventListener('submit', onSubmit);
 
-function onSubmit(e){
+function onSubmit(e) {
     e.preventDefault();
 
-    if(firstName.value === '' || lastName.value === ''){
+    if (firstName.value === '' || lastName.value === '') {
         alert('Please enter fields !');
-     }
-     else{
+    }
+    else {
         // localStorage.setItem('FirstName', firstName.value);
         // localStorage.setItem('LastName', lastName.value);
         // localStorage.setItem('Gender', genderInput.value);
@@ -25,24 +24,56 @@ function onSubmit(e){
         const userDetails = {
             firstName: firstName.value,
             lastName: lastName.value,
-            gender: genderInput.value,
+            gender: genderInput.checked ? 'Male' : 'Female',
             date: dateInput.value,
             contact: contactInput.value
         };
 
         let userDetails_serialized = JSON.stringify(userDetails);
 
-        localStorage.setItem('userDetails', userDetails_serialized);
+        localStorage.setItem(firstName.value, userDetails_serialized);
 
         // let userDetails_deserialized = JSON.parse(localStorage.getItem('userDetails'));
-        
-        
-        const user = document.createElement('p');
 
-        user.textContent = userDetails_serialized;
-        userBox.appendChild(user);
+        const user = document.createElement('li');
 
+        user.innerHTML =
+            userDetails.firstName + ' ' +
+            userDetails.lastName + ' ' +
+            userDetails.gender + ' ' +
+            userDetails.date + ' ' +
+            userDetails.contact;
 
+        //Delet Button
+        const userKey = firstName.value;       
+        const deleteBtn = document.createElement('input');
+        deleteBtn.type = 'button';
+        deleteBtn.value = 'Delete';
 
-     }
+        //styling Delete Button
+        deleteBtn.style.backgroundColor = 'red';
+        deleteBtn.style.color = 'white';
+        deleteBtn.style.border = 'none';
+        deleteBtn.style.borderRadius = '3px';
+        deleteBtn.style.position = 'absolute';
+        deleteBtn.style.right = '0';
+        deleteBtn.style.padding = '5px';
+        deleteBtn.style.marginRight = '5px';
+
+        deleteBtn.onclick = function () {
+            user.remove();
+            localStorage.removeItem(userKey);
+        };
+
+        user.appendChild(deleteBtn);
+        userList.appendChild(user);
+
+        //clearing the inputs 
+        firstName.value = '';
+        lastName.value = '';
+        genderInput.checked = false;
+        dateInput.value = '';
+        contactInput.value = '';
+
+    }
 }
